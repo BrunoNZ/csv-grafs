db3399d2e31e5ead31af9021544065caff366104
Bruno Nocera Zanette <brunonzanette@gmail.com>
2022-07-29 23:13:51 -0300

    Ajuste

--- execute_all.py
+++ execute_all.py
@@ -4,14 +4,15 @@ import sys
 import multiprocessing
 from multiprocessing.pool import ThreadPool
 from os import listdir
-from os.path import join
+from os.path import join, dirname
 import subprocess
 
+CSV_GRAFS = join(dirname(__file__), "main.py")
 POPEN_OPTS = {"stdout": subprocess.PIPE, "stderr": subprocess.DEVNULL}
 
 
 def call_proc(cmd):
-    with subprocess.Popen(cmd, **POPEN_OPTS) as cmd_sp:
+    with subprocess.Popen([CSV_GRAFS] + cmd, **POPEN_OPTS) as cmd_sp:
         out, err = cmd_sp.communicate()
         print("OK: ", " ".join(cmd))
         return (out, err)
@@ -19,11 +20,10 @@ def call_proc(cmd):
 
 def main():
     args = sys.argv[1:]
-    csvgraf = args[0]
-    defs_json = args[1]
-    input_dir = args[2]
-    inside_dir = args[3]
-    output_dir = args[4]
+    defs_json = args[0]
+    input_dir = args[1]
+    inside_dir = args[2]
+    output_dir = args[3]
 
     results = listdir(input_dir)
     results_dir = map(lambda f: [f, join(input_dir, f, inside_dir)], results)
@@ -32,7 +32,7 @@ def main():
     with ThreadPool(multiprocessing.cpu_count()) as pool:
         for rname, dir_in in results_dir:
             dir_out = join(output_dir, rname)
-            args = [csvgraf, defs_json, "-i", dir_in, "-o", dir_out]
+            args = [defs_json, "-i", dir_in, "-o", dir_out]
             thread_pool.append(pool.apply_async(call_proc, (args,)))
         pool.close()
         pool.join()
